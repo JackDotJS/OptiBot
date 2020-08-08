4bf6e82520b96411ea4d0a5cfd2171453a4f0973
realjackasterisk <wingedasterisk@gmail.com>
2020-08-08 00:04:57 -0700

    test

--- modules/cmd/ping.js
+++ modules/cmd/ping.js
@@ -21,6 +21,8 @@ metadata.run = (m, args, data) => {
     .setColor(bot.cfg.embed.default)
     .setDescription(`API Latency: ... \nMessage Latency: ...`)
 
+    throw new Error('test');
+
     let timeStart = new Date().getTime();
     m.channel.send('_ _', {embed: embed}).then(msg => {
         let timeTaken = new Date().getTime() - timeStart;

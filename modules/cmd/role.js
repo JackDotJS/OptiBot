92bea72451e4a1826d5fff8c62657e8ab4bbd519
akii0008 <jennasisis@gmail.com>
2020-09-13 11:08:24 -0400

    cmd/role: cleaned, removed useless requires

--- modules/cmd/role.js
+++ modules/cmd/role.js
@@ -1,8 +1,7 @@
 const path = require('path');
-const util = require('util');
 const djs = require('discord.js');
 const sim = require('string-similarity');
-const { Command, OBUtil, Memory, RecordEntry, LogEntry, Assets } = require('../core/OptiBot.js');
+const { Command, OBUtil, Memory, LogEntry, Assets } = require('../core/OptiBot.js');
 
 const bot = Memory.core.client;
 const log = bot.log;
@@ -19,86 +18,81 @@ const metadata = {
 };
 
 metadata.run = (m, args, data) => {
-  if(!args[1]) {
-    OBUtil.missingArgs(m, metadata);
-  } else {
-    OBUtil.parseTarget(m, 0, args[0], data.member).then((result) => {
-      if (!result) {
-        OBUtil.err('You must specify a valid user.', {m:m});
-      } else
-      if (result.type === 'notfound' || result.type === 'id' || result.type === 'user') {
-        OBUtil.err('Unable to find a user.', {m:m});
-      } else
-      if (result.target.user.id === m.author.id || result.target.user.id === bot.user.id) {
-        OBUtil.err('Nice try.', {m:m});
-      } else 
-      if (OBUtil.getAuthlvl(result.target) > data.authlvl) {
-        OBUtil.err('You aren\'t powerful enough to update this user\'s roles.', {m:m});
-      } else {
-        const roles = [...bot.guilds.cache.get(bot.cfg.guilds.optifine).roles.cache.filter(role => bot.cfg.roles.grantable.indexOf(role.id) > -1).values()];
-        const match = {
-          role: null,
-          rating: 0,
-        };
-        const reqrole = m.content.substring((`${bot.prefix}${data.input.cmd} ${args[0]} `).length);
-        for(const role of roles) {
-          const newrating = sim.compareTwoStrings(reqrole, role.name);
-          if (newrating > match.rating) {
-            match.role = role;
-            match.rating = newrating;
-          }
-        }
+  if (!args[1]) return OBUtil.missingArgs(m, metadata);
 
-        log(match);
-
-        if (match.rating < 0.05) {
-          OBUtil.err('What kind of role is that?', {m:m});
-        } else
-        if (!result.target.roles.cache.has(match.role.id)) {
-          result.target.roles.add(match.role.id, `Role granted by ${m.author.tag}`).then(() => {
-            const logEntry = new LogEntry({channel: 'moderation'})
-              .setColor(bot.cfg.embed.okay)
-              .setIcon(Assets.getEmoji('ICO_join').url)
-              .setTitle('Member Role Granted', 'Member Role Grant Report')
-              .setThumbnail(result.target.user.displayAvatarURL({format:'png'}))
-              .addSection('Member', result.target)
-              .addSection('Moderator Responsible', m.author)
-              .addSection('Command Location', m)
-              .addSection('Role', `${match.role}`)
-              .submit().then(() => {
-                const embed = new djs.MessageEmbed()
-                  .setColor(bot.cfg.embed.okay)
-                  .setAuthor('Role added', Assets.getEmoji('ICO_okay').url)
-                  .setDescription(`${result.target} has been given the ${match.role} role.`);
-
-                m.channel.send({embed: embed}).then(bm => OBUtil.afterSend(bm, m.author.id));
-              });
-          }).catch(err => OBUtil.err(err, {m:m}));
-        } else {
-          result.target.roles.remove(match.role.id, `Role removed by ${m.author.tag}`).then(() => {
-            const logEntry = new LogEntry({channel: 'moderation'})
-              .setColor(bot.cfg.embed.error)
-              .setIcon(Assets.getEmoji('ICO_leave').url)
-              .setTitle('Member Role Removed', 'Member Role Removal Report')
-              .setThumbnail(result.target.user.displayAvatarURL({format:'png'}))
-              .addSection('Member', result.target)
-              .addSection('Moderator Responsible', m.author)
-              .addSection('Command Location', m)
-              .addSection('Role', `${match.role}`)
-              .submit().then(() => {
-                const embed = new djs.MessageEmbed()
-                  .setColor(bot.cfg.embed.okay)
-                  .setAuthor('Role removed', Assets.getEmoji('ICO_okay').url)
-                  .setDescription(`${result.target} no longer has the ${match.role} role.`);
-
-                m.channel.send({embed: embed}).then(bm => OBUtil.afterSend(bm, m.author.id));
-              });
-                        
-          }).catch(err => OBUtil.err(err, {m:m}));
+  OBUtil.parseTarget(m, 0, args[0], data.member).then((result) => {
+    if (!result) {
+      OBUtil.err('You must specify a valid user.', { m });
+    } else if (result.type === 'notfound' || result.type === 'id' || result.type === 'user') {
+      OBUtil.err('Unable to find a user.', { m });
+    } else if (result.target.user.id === m.author.id || result.target.user.id === bot.user.id) {
+      OBUtil.err('Nice try.', { m });
+    } else if (OBUtil.getAuthlvl(result.target) > data.authlvl) {
+      OBUtil.err('You aren\'t powerful enough to update this user\'s roles.', { m });
+    } else {
+      const roles = [...bot.guilds.cache.get(bot.cfg.guilds.optifine).roles.cache.filter(role => bot.cfg.roles.grantable.indexOf(role.id) > -1).values()];
+      const match = {
+        role: null,
+        rating: 0,
+      };
+      const reqrole = m.content.substring((`${bot.prefix}${data.input.cmd} ${args[0]} `).length);
+      for (const role of roles) {
+        const newrating = sim.compareTwoStrings(reqrole, role.name);
+        if (newrating > match.rating) {
+          match.role = role;
+          match.rating = newrating;
         }
       }
-    }).catch(err => OBUtil.err(err, {m:m}));
-  }
+
+      log(match);
+
+      if (match.rating < 0.05) {
+        OBUtil.err('What kind of role is that?', { m });
+      } else if (!result.target.roles.cache.has(match.role.id)) {
+        result.target.roles.add(match.role.id, `Role granted by ${m.author.tag}`).then(() => {
+          new LogEntry({ channel: 'moderation' })
+            .setColor(bot.cfg.embed.okay)
+            .setIcon(Assets.getEmoji('ICO_join').url)
+            .setTitle('Member Role Granted', 'Member Role Grant Report')
+            .setThumbnail(result.target.user.displayAvatarURL({ format: 'png' }))
+            .addSection('Member', result.target)
+            .addSection('Moderator Responsible', m.author)
+            .addSection('Command Location', m)
+            .addSection('Role', `${match.role}`)
+            .submit().then(() => {
+              const embed = new djs.MessageEmbed()
+                .setColor(bot.cfg.embed.okay)
+                .setAuthor('Role added', Assets.getEmoji('ICO_okay').url)
+                .setDescription(`${result.target} has been given the ${match.role} role.`);
+
+              m.channel.send({ embed: embed }).then(bm => OBUtil.afterSend(bm, m.author.id));
+            });
+        }).catch(err => OBUtil.err(err, { m }));
+      } else {
+        result.target.roles.remove(match.role.id, `Role removed by ${m.author.tag}`).then(() => {
+          new LogEntry({ channel: 'moderation' })
+            .setColor(bot.cfg.embed.error)
+            .setIcon(Assets.getEmoji('ICO_leave').url)
+            .setTitle('Member Role Removed', 'Member Role Removal Report')
+            .setThumbnail(result.target.user.displayAvatarURL({ format: 'png' }))
+            .addSection('Member', result.target)
+            .addSection('Moderator Responsible', m.author)
+            .addSection('Command Location', m)
+            .addSection('Role', `${match.role}`)
+            .submit().then(() => {
+              const embed = new djs.MessageEmbed()
+                .setColor(bot.cfg.embed.okay)
+                .setAuthor('Role removed', Assets.getEmoji('ICO_okay').url)
+                .setDescription(`${result.target} no longer has the ${match.role} role.`);
+
+              m.channel.send({ embed: embed }).then(bm => OBUtil.afterSend(bm, m.author.id));
+            });
+
+        }).catch(err => OBUtil.err(err, { m }));
+      }
+    }
+  }).catch(err => OBUtil.err(err, { m }));
+
 };
 
 module.exports = new Command(metadata);

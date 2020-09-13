26782ff86041ed1b050ca0f1cf467c3ad90bce5f
akii0008 <jennasisis@gmail.com>
2020-09-12 22:43:31 -0400

    OptiBotUtil: disabled eslint no-useless-escape on regex

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -426,6 +426,7 @@ module.exports = class OptiBotUtilities {
       } else 
       if(target.match(/discordapp\.com|discord.com/i)) {
         log('url');
+        // eslint-disable-next-line no-useless-escape
         const urls = m.content.match(/\b((?:[a-z][\w-]+:(?:\/{1,3}|[a-z0-9%])|www\d{0,3}[.]|[a-z0-9.\-]+[.][a-z]{2,4}\/)(?:[^\s()<>]+|\(([^\s()<>]+|(\([^\s()<>]+\)))*\))+(?:\(([^\s()<>]+|(\([^\s()<>]+\)))*\)|[^\s`!()\[\]{};:'".,<>?«»“”‘’]))/gi);
     
         if(urls !== null) {

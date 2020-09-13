c8dd3d28201b330704d74b474930539c22dc321e
akii0008 <jennasisis@gmail.com>
2020-09-12 22:55:22 -0400

    index: cleaned

--- index.js
+++ index.js
@@ -115,7 +115,7 @@ bot.on('ready', () => {
         log(centerText('  ', width), 'info');
         log(`╰${'─'.repeat(width)}╯`, 'info');
 
-        const logEntry = new ob.LogEntry({ time: now, console: false })
+        new ob.LogEntry({ time: now, console: false })
           .setColor(bot.cfg.embed.default)
           .setIcon(ob.Assets.getEmoji('ICO_info').url)
           .setThumbnail(bot.user.displayAvatarURL({ format: 'png' }))
@@ -193,7 +193,7 @@ bot.on('message', (m) => {
     if (authlvl < 4 && bot.mode === 0 && m.author.id !== '271760054691037184') return;
     if (authlvl < 1 && bot.mode === 1 && m.author.id !== '271760054691037184') return;
 
-    if(input.valid) {
+    if (input.valid) {
       /////////////////////////////////////////////////////////////
       // COMMAND HANDLER
       /////////////////////////////////////////////////////////////
@@ -933,7 +933,7 @@ bot.on('guildMemberRemove', member => {
       for (let i = 0; i < ad.length; i++) {
         if ((ad[i].action === 'MEMBER_KICK' || ad[i].action === 'MEMBER_BAN_ADD') && ad[i].target.id === member.user.id) {
           if (ad[i].action === 'MEMBER_KICK') {
-            const logEntry = new ob.LogEntry({ time: now, channel: 'moderation' })
+            new ob.LogEntry({ time: now, channel: 'moderation' })
               .setColor(bot.cfg.embed.error)
               .setIcon(ob.Assets.getEmoji('ICO_leave').url)
               .setThumbnail(member.user.displayAvatarURL({ format: 'png' }))
@@ -946,7 +946,7 @@ bot.on('guildMemberRemove', member => {
           break;
         } else
         if (i + 1 >= ad.length) {
-          const logEntry = new ob.LogEntry({ time: now, channel: 'joinleave' })
+          new ob.LogEntry({ time: now, channel: 'joinleave' })
             .setColor(bot.cfg.embed.error)
             .setIcon(ob.Assets.getEmoji('ICO_leave').url)
             .setThumbnail(member.user.displayAvatarURL({ format: 'png' }))
@@ -1409,112 +1409,80 @@ bot.on('shardReady', (id, guilds) => {
 ////////////////////////////////////////
 
 bot.on('shardDisconnect', (event, id) => {
-  function getCodeName(code) {
-    if (code >= 0 && code <= 999) {
-      return 'Reserved';
-    } else
-    if (code === 1000) {
-      return 'Normal Closure';
-    } else
-    if (code === 1001) {
-      return 'Going Away';
-    } else
-    if (code === 1002) {
-      return 'Protocol Error';
-    } else
-    if (code === 1003) {
-      return 'Unsupported Data';
-    } else
-    if (code === 1004) {
-      return 'Reserved';
-    } else
-    if (code === 1005) {
-      return 'No Status Received';
-    } else
-    if (code === 1006) {
-      return 'Abnormal Closure';
-    } else
-    if (code === 1007) {
-      return 'Invalid Frame Payload Data';
-    } else
-    if (code === 1008) {
-      return 'Policy Violation';
-    } else
-    if (code === 1009) {
-      return 'Message Too Big';
-    } else
-    if (code === 1010) {
-      return 'Missing Extension';
-    } else
-    if (code === 1011) {
-      return 'Internal Error';
-    } else
-    if (code === 1012) {
-      return 'Service Restart';
-    } else
-    if (code === 1013) {
-      return 'Try Again Later';
-    } else
-    if (code === 1014) {
-      return 'Bad Gateway';
-    } else
-    if (code === 1015) {
-      return 'TLS Handshake';
-    } else
-    if (code >= 1016 && code <= 3999) {
-      return 'Reserved';
-    } else
-    if (code === 4000) {
-      return 'DISCORD: Unknown Error';
-    } else
-    if (code === 4001) {
-      return 'DISCORD: Unknown Opcode';
-    } else
-    if (code === 4002) {
-      return 'DISCORD: Decode Error';
-    } else
-    if (code === 4003) {
-      return 'DISCORD: Not Authenticated';
-    } else
-    if (code === 4004) {
-      return 'DISCORD: Authentication Failed';
-    } else
-    if (code === 4005) {
-      return 'DISCORD: Already Authenticated';
-    } else
-    // there is no code 4006 for some reason
-    // https://discordapp.com/developers/docs/topics/opcodes-and-status-codes
-    if (code === 4007) {
-      return 'DISCORD: Invalid Sequence';
-    } else
-    if (code === 4008) {
-      return 'DISCORD: Rate Limited';
-    } else
-    if (code === 4009) {
-      return 'DISCORD: Session Timed Out';
-    } else
-    if (code === 4010) {
-      return 'DISCORD: Invalid Shard';
-    } else
-    if (code === 4011) {
-      return 'DISCORD: Sharding Required';
-    } else
-    if (code === 4012) {
-      return 'DISCORD: Invalid API Version';
-    } else
-    if (code === 4013) {
-      return 'DISCORD: Invalid Intent';
-    } else
-    if (code === 4014) {
-      return 'DISCORD: Disallowed Intent';
-    } else {
-      return 'Unknown';
-    }
-  }
-
   log(`Shard WebSocket disconnected. \nShard ID: ${id} \nEvent Code: ${event.code} (${getCodeName(event.code)})`, 'warn');
   ob.OBUtil.setWindowTitle();
 });
+
+function getCodeName(code) {
+  if (code >= 0 && code <= 999) {
+    return 'Reserved';
+  } else if (code === 1000) {
+    return 'Normal Closure';
+  } else if (code === 1001) {
+    return 'Going Away';
+  } else if (code === 1002) {
+    return 'Protocol Error';
+  } else if (code === 1003) {
+    return 'Unsupported Data';
+  } else if (code === 1004) {
+    return 'Reserved';
+  } else if (code === 1005) {
+    return 'No Status Received';
+  } else if (code === 1006) {
+    return 'Abnormal Closure';
+  } else if (code === 1007) {
+    return 'Invalid Frame Payload Data';
+  } else if (code === 1008) {
+    return 'Policy Violation';
+  } else if (code === 1009) {
+    return 'Message Too Big';
+  } else if (code === 1010) {
+    return 'Missing Extension';
+  } else if (code === 1011) {
+    return 'Internal Error';
+  } else if (code === 1012) {
+    return 'Service Restart';
+  } else if (code === 1013) {
+    return 'Try Again Later';
+  } else if (code === 1014) {
+    return 'Bad Gateway';
+  } else if (code === 1015) {
+    return 'TLS Handshake';
+  } else if (code >= 1016 && code <= 3999) {
+    return 'Reserved';
+  } else if (code === 4000) {
+    return 'DISCORD: Unknown Error';
+  } else if (code === 4001) {
+    return 'DISCORD: Unknown Opcode';
+  } else if (code === 4002) {
+    return 'DISCORD: Decode Error';
+  } else if (code === 4003) {
+    return 'DISCORD: Not Authenticated';
+  } else if (code === 4004) {
+    return 'DISCORD: Authentication Failed';
+  } else if (code === 4005) {
+    return 'DISCORD: Already Authenticated';
+    // there is no code 4006 for some reason https://discordapp.com/developers/docs/topics/opcodes-and-status-codes
+  } else if (code === 4007) {
+    return 'DISCORD: Invalid Sequence';
+  } else if (code === 4008) {
+    return 'DISCORD: Rate Limited';
+  } else if (code === 4009) {
+    return 'DISCORD: Session Timed Out';
+  } else if (code === 4010) {
+    return 'DISCORD: Invalid Shard';
+  } else if (code === 4011) {
+    return 'DISCORD: Sharding Required';
+  } else if (code === 4012) {
+    return 'DISCORD: Invalid API Version';
+  } else if (code === 4013) {
+    return 'DISCORD: Invalid Intent';
+  } else if (code === 4014) {
+    return 'DISCORD: Disallowed Intent';
+  } else {
+    return 'Unknown';
+  }
+}
 
 ////////////////////////////////////////
 // Shard Reconnecting

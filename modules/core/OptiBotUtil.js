0a9a47f2249d71376f7f5793e72d7e61da65f8bb
akii0008 <jennasisis@gmail.com>
2020-09-13 15:13:26 -0400

    moved confirm into its own file

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -11,59 +11,13 @@ module.exports = {
   missingArgs: require('./OptiBotUtil/missingArgs'),
   getProfile: require('./OptiBotUtil/getProfile'),
   updateProfile: require('./OptiBotUtil/updateProfile'),
-  parseTarget: require('./OptiBotUtil/parseTarget')
+  parseTarget: require('./OptiBotUtil/parseTarget'),
+  confirm: require('./OptiBotUtil/confirm')
 };
 
 module.exports = class OptiBotUtilities {
   constructor() {
     throw new Error('Why are you doing this? (Cannot instantiate this class.)');
-  }
-
-  static confirm(m, bm) {
-    const bot = Memory.core.client;
-    const log = bot.log;
-
-    return new Promise((resolve, reject) => {
-      m.channel.stopTyping(true);
-
-      const filter = (r, user) => [bot.cfg.emoji.confirm, bot.cfg.emoji.cancel].indexOf(r.emoji.id) > -1 && user.id === m.author.id;
-      const df = bm.createReactionCollector(filter, { time: (1000 * 60 * 5) });
-
-      df.on('collect', r => {
-        df.stop('done');
-
-        if (r.emoji.id === bot.cfg.emoji.confirm) {
-          resolve(1);
-        } else {
-          resolve(0);
-        }
-      });
-
-      df.on('end', (c, reason) => {
-        if (!bm.deleted) {
-          bm.reactions.removeAll().then(() => {
-            if (reason === 'done') {
-              return;
-            } else if (reason === 'time') {
-              resolve(-1);
-            } else {
-              log(reason, 'error');
-            }
-          });
-        }
-      });
-
-      const ob = bot.guilds.cache.get(bot.cfg.guilds.optibot);
-      bm.react(ob.emojis.cache.get(bot.cfg.emoji.confirm)).then(() => {
-        bm.react(ob.emojis.cache.get(bot.cfg.emoji.cancel)).catch(err => {
-          df.stop();
-          reject(err);
-        });
-      }).catch(err => {
-        df.stop();
-        reject(err);
-      });
-    });
   }
 
   /**

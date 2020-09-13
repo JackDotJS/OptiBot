6a60e35465c1a7ffc86fa033d05a0390341d344c
akii0008 <jennasisis@gmail.com>
2020-09-13 10:30:34 -0400

    cmd/jarfix: cleaned, removed useless requires

--- modules/cmd/jarfix.js
+++ modules/cmd/jarfix.js
@@ -1,9 +1,8 @@
 const path = require('path');
 const djs = require('discord.js');
-const { Command, OBUtil, Memory, RecordEntry, LogEntry, Assets } = require('../core/OptiBot.js');
+const { Command, OBUtil, Memory, Assets } = require('../core/OptiBot.js');
 
 const bot = Memory.core.client;
-const log = bot.log;
 
 const metadata = {
   name: path.parse(__filename).name,
@@ -14,7 +13,7 @@ const metadata = {
   run: null
 };
 
-metadata.run = (m, args, data) => {
+metadata.run = m => {
   const embed = new djs.MessageEmbed()
     .setColor(bot.cfg.embed.default)
     .setAuthor('Download Jarfix', Assets.getEmoji('ICO_jarfix').url)
@@ -22,7 +21,7 @@ metadata.run = (m, args, data) => {
     .addField('Direct Download', 'https://johann.loefflmann.net/downloads/jarfix.exe')
     .setFooter(`Jarfix Copyright © 2002-${new Date().getUTCFullYear()} by Dipl.-Inf. (FH) Johann Nepomuk Löfflmann`);
 
-  m.channel.send({ embed: embed }).then(bm => OBUtil.afterSend(bm, m.author.id));
+  m.channel.send(embed).then(bm => OBUtil.afterSend(bm, m.author.id));
 };
 
 module.exports = new Command(metadata);

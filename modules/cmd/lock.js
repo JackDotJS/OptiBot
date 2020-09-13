2306ad3f78331f54e40dc0a2671fb0fb3f3ee048
akii0008 <jennasisis@gmail.com>
2020-09-13 10:38:42 -0400

    cmd/lock: cleaned, removed useless requires

--- modules/cmd/lock.js
+++ modules/cmd/lock.js
@@ -1,10 +1,8 @@
 const path = require('path');
-const util = require('util');
 const djs = require('discord.js');
-const { Command, OBUtil, Memory, RecordEntry, LogEntry, Assets } = require('../core/OptiBot.js');
+const { Command, OBUtil, Memory, LogEntry, Assets } = require('../core/OptiBot.js');
 
 const bot = Memory.core.client;
-const log = bot.log;
 
 const metadata = {
   name: path.parse(__filename).name,
@@ -20,25 +18,24 @@ const metadata = {
 metadata.run = (m, args, data) => {
   let channel = m.channel;
 
-  if(args[0]) {
-    if(djs.MessageMentions.CHANNELS_PATTERN.exec(args[0]) != null) {
+  if (args[0]) {
+    if (djs.MessageMentions.CHANNELS_PATTERN.exec(args[0]) != null) {
       channel = args[0].mentions.channels.first();
-    } else
-    if(parseInt(args[0]) >= 1420070400000) {
+    } else if (parseInt(args[0]) >= 1420070400000) {
       channel = bot.channels.cache.get(args[0]) || m.channel;
     }
-  } 
+  }
 
   if (bot.cfg.channels.nomodify.some(id => [channel.id, channel.parentID].includes(id)) || channel.guild.id != bot.mainGuild.id) {
-    return OBUtil.err(`The #${channel.name} channel cannot be modified.`, {m:m});
+    return OBUtil.err(`The #${channel.name} channel cannot be modified.`, { m: m });
   }
 
   if (channel.permissionOverwrites.get(bot.mainGuild.id).deny.has('SEND_MESSAGES')) {
-    return OBUtil.err(`The #${channel.name} channel has already been locked.`, {m:m});
+    return OBUtil.err(`The #${channel.name} channel has already been locked.`, { m: m });
   }
 
-  channel.updateOverwrite(bot.mainGuild.id, {SEND_MESSAGES:false}, `Channel locked by ${m.author.tag} (${m.author.id}) via ${bot.prefix}${data.input.cmd}`).then(() => {
-    const logEntry = new LogEntry({channel: 'moderation'})
+  channel.updateOverwrite(bot.mainGuild.id, { SEND_MESSAGES: false }, `Channel locked by ${m.author.tag} (${m.author.id}) via ${bot.prefix}${data.input.cmd}`).then(() => {
+    const logEntry = new LogEntry({ channel: 'moderation' })
       .setColor(bot.cfg.embed.default)
       .setIcon(Assets.getEmoji('ICO_lock').url)
       .setTitle('Channel Locked', 'Channel Lock Report')
@@ -52,7 +49,7 @@ metadata.run = (m, args, data) => {
       .setDescription(`The ${channel} channel has been locked.`);
 
     m.channel.stopTyping(true);
-    m.channel.send({embed: embed});//.then(bm => OBUtil.afterSend(bm, m.author.id));
+    m.channel.send(embed);//.then(bm => OBUtil.afterSend(bm, m.author.id));
     logEntry.submit();
   });
 };

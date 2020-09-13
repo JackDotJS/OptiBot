ab2aacc7b833fc6398494610f5209ed8a577f55c
akii0008 <jennasisis@gmail.com>
2020-09-13 10:14:47 -0400

    cmd/die: cleaned, removed useless requires

--- modules/cmd/die.js
+++ modules/cmd/die.js
@@ -1,10 +1,5 @@
 const path = require('path');
-const util = require('util');
-const djs = require('discord.js');
-const { Command, OBUtil, Memory, RecordEntry, LogEntry, Assets } = require('../core/OptiBot.js');
-
-const bot = Memory.core.client;
-const log = bot.log;
+const { Command, OBUtil, Assets } = require('../core/OptiBot.js');
 
 const metadata = {
   name: path.parse(__filename).name,
@@ -15,8 +10,6 @@ const metadata = {
   run: null
 };
 
-metadata.run = (m, args, data) => {
-  m.channel.send(Assets.getImage('IMG_marcelo_die').attachment).then(bm => OBUtil.afterSend(bm, m.author.id));
-};
+metadata.run = m => m.channel.send(Assets.getImage('IMG_marcelo_die').attachment).then(bm => OBUtil.afterSend(bm, m.author.id));
 
 module.exports = new Command(metadata);

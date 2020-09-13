ed4a9e50dba5289634ddb9c9a03b01bb4987497d
akii0008 <jennasisis@gmail.com>
2020-09-13 15:27:28 -0400

    moved calculatePoints to its own file

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -1,4 +1,3 @@
-const Memory = require('./OptiBotMemory.js');
 
 module.exports = {
   setWindowTitle: require('./OptiBotUtil/setWindowTitle'),
@@ -14,32 +13,6 @@ module.exports = {
   afterSend: require('./OptiBotUtil/afterSend'),
   unmuter: require('./OptiBotUtil/unmuter'),
   uwu: require('./OptiBotUtil/uwu'),
-  verifyDonator: require('./OptiBotUtil/verifyDonator')
-};
-
-module.exports = class OptiBotUtilities {
-  constructor() {
-    throw new Error('Why are you doing this? (Cannot instantiate this class.)');
-  }
-
-  static calculatePoints(date, total) {
-    const bot = Memory.core.client;
-    const log = bot.log;
-
-    const daysSince = Math.round((Date.now() - date) / (1000 * 60 * 60 * 24));
-
-    log(`days since: ${daysSince}`);
-
-    if (daysSince > bot.cfg.points.decayDelay) {
-      const decay = total - (bot.cfg.points.dailyDecay * (daysSince - bot.cfg.points.decayDelay));
-      const minimum = (bot.cfg.points.minPercentDecay / 100) * total;
-
-      log(`decay: ${decay}`);
-      log(`minimum: ${minimum}`);
-
-      return Math.max(decay, minimum);
-    }
-
-    return total;
-  }
+  verifyDonator: require('./OptiBotUtil/verifyDonator'),
+  calculatePoints: require('./OptiBotUtil/calculatePoints')
 };

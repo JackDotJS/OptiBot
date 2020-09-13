44e12062c6a36a22cfbf75e3787cc42607143fee
akii0008 <jennasisis@gmail.com>
2020-09-13 14:57:57 -0400

    moved parseInput into its own file

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -7,31 +7,13 @@ const Memory = require('./OptiBotMemory.js');
 const Assets = require('./OptiBotAssetsManager.js');
 
 module.exports = {
-  setWindowTitle: require('./OptiBotUtil/setWindowTitle')
+  setWindowTitle: require('./OptiBotUtil/setWindowTitle'),
+  parseInput: require('./OptiBotUtil/parseInput')
 };
 
 module.exports = class OptiBotUtilities {
   constructor() {
     throw new Error('Why are you doing this? (Cannot instantiate this class.)');
-  }
-
-  static parseInput(text) {
-    const bot = Memory.core.client;
-
-    if (typeof text !== 'string') text = new String(text);
-    const input = text.trim().split('\n', 1)[0]; // first line of the message
-    const data = {
-      valid: input.match(new RegExp(`^(\\${bot.prefixes.join('|\\')})(?![^a-zA-Z0-9])[a-zA-Z0-9]+(?=\\s|$)`)), // checks if the input starts with the command prefix, immediately followed by valid characters.
-      cmd: input.toLowerCase().split(' ')[0].substr(1),
-      args: input.split(' ').slice(1).filter(function (e) { return e.length !== 0; })
-    };
-
-    if (input.match(/^(\$)(?![^0-9])[0-9]+(?=\s|$)/)) {
-      // fixes "$[numbers]" resulting in false command inputs
-      data.valid = null;
-    }
-
-    return data;
   }
 
   static getAuthlvl(member, ignoreElevated) {
---
+++ modules/core/OptiBotUtil/parseInput.js
@@ -0,0 +1,20 @@
+const Memory = require('../OptiBotMemory.js');
+
+module.exports = (text) => {
+  const bot = Memory.core.client;
+
+  if (typeof text !== 'string') text = new String(text);
+  const input = text.trim().split('\n', 1)[0]; // first line of the message
+  const data = {
+    valid: input.match(new RegExp(`^(\\${bot.prefixes.join('|\\')})(?![^a-zA-Z0-9])[a-zA-Z0-9]+(?=\\s|$)`)), // checks if the input starts with the command prefix, immediately followed by valid characters.
+    cmd: input.toLowerCase().split(' ')[0].substr(1),
+    args: input.split(' ').slice(1).filter(function (e) { return e.length !== 0; })
+  };
+
+  if (input.match(/^(\$)(?![^0-9])[0-9]+(?=\s|$)/)) {
+    // fixes "$[numbers]" resulting in false command inputs
+    data.valid = null;
+  }
+
+  return data;
+};

0a888ecdfd7c4d6325f05e55cc68016241219605
akii0008 <jennasisis@gmail.com>
2020-09-13 15:04:12 -0400

    moved getProfile into its own file

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -9,38 +9,13 @@ module.exports = {
   setWindowTitle: require('./OptiBotUtil/setWindowTitle'),
   parseInput: require('./OptiBotUtil/parseInput'),
   getAuthlvl: require('./OptiBotUtil/getAuthLevel'),
-  missingArgs: require('./OptiBotUtil/missingArgs')
+  missingArgs: require('./OptiBotUtil/missingArgs'),
+  getProfile: require('./OptiBotUtil/getProfile')
 };
 
 module.exports = class OptiBotUtilities {
   constructor() {
     throw new Error('Why are you doing this? (Cannot instantiate this class.)');
-  }
-
-  /**
-     * @param {OptiBot} bot OptiBot client.
-     * @param {String} id Profile ID to search for.
-     * @param {Boolean} [create] If true, and a profile with the given id does not exist, create a new profile. Otherwise, return null.
-     * @returns {Promise<Profile>|Promise<null>}
-     */
-  static getProfile(id, create) {
-    const bot = Memory.core.client;
-    const log = bot.log;
-
-    return new Promise((resolve, reject) => {
-      log('get profile: ' + id);
-      Memory.db.profiles.find({ id: id, format: 3 }, (err, docs) => {
-        if (err) {
-          reject(err);
-        } else if (docs[0]) {
-          resolve(new Profile(docs[0]));
-        } else if (create) {
-          resolve(new Profile({ id: id }));
-        } else {
-          resolve();
-        }
-      });
-    });
   }
 
   static updateProfile(data) {

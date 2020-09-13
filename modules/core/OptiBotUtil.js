7ec9a95e3b98810397b933ebb2ffbaddbd38bf72
akii0008 <jennasisis@gmail.com>
2020-09-13 15:23:30 -0400

    moved unmuter into tis own file

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -1,8 +1,4 @@
-const util = require('util');
-const djs = require('discord.js');
-const LogEntry = require('./OptiBotLogEntry.js');
 const Memory = require('./OptiBotMemory.js');
-const Assets = require('./OptiBotAssetsManager.js');
 
 module.exports = {
   setWindowTitle: require('./OptiBotUtil/setWindowTitle'),
@@ -15,126 +11,13 @@ module.exports = {
   confirm: require('./OptiBotUtil/confirm'),
   err: require('./OptiBotUtil/err'),
   parseTime: require('./OptiBotUtil/parseTime'),
-  afterSend: require('./OptiBotUtil/afterSend')
+  afterSend: require('./OptiBotUtil/afterSend'),
+  unmuter: require('./OptiBotUtil/unmuter')
 };
 
 module.exports = class OptiBotUtilities {
   constructor() {
     throw new Error('Why are you doing this? (Cannot instantiate this class.)');
-  }
-
-  static unmuter(id) {
-    const bot = Memory.core.client;
-    const log = bot.log;
-
-    // eslint-disable-next-line no-unused-vars
-    return new Promise((resolve, reject) => {
-      const errHandler = (err, user) => {
-        OptiBotUtilities.err(err);
-
-        let type = null;
-        if (user) type = (user.constructor === djs.User) ? 'user' : 'member';
-
-        const logEntry = new LogEntry({ channel: 'moderation' })
-          .setColor(bot.cfg.embed.error)
-          .setIcon(Assets.getEmoji('ICO_error').url)
-          .setTitle('Member Unmute Failure', 'Member Mute Removal Failure Report')
-          .setHeader('An error occurred while trying to unmute a user.')
-          .setDescription(`\`\`\`diff\n-${err}\`\`\``);
-
-        if (user) {
-          logEntry.addSection('Member', (type === 'user') ? user : user.user)
-            .setThumbnail(((type === 'user') ? user : user.user).displayAvatarURL({ format: 'png' }));
-        } else {
-          logEntry.addSection('Member', 'Unknown. (Error occurred before or during fetch operation)');
-        }
-
-        logEntry.submit().then(() => {
-          resolve();
-        }).catch(err => {
-          OptiBotUtilities.err(err);
-          resolve();
-        });
-      };
-
-      bot.mainGuild.members.fetch(id).then(mem => {
-        removeRole(mem);
-      }).catch(err => {
-        if (err.message.match(/invalid or uncached|unknown member|unknown user/i)) {
-          bot.users.fetch(id).then(user => {
-            removeRole(user);
-          }).catch(err => {
-            errHandler(err);
-          });
-        } else {
-          errHandler(err);
-        }
-      });
-
-      function removeRole(user) {
-        if (user.constructor === djs.User) {
-          removeProfileData(user, 'user');
-        } else {
-          user.roles.remove(bot.cfg.roles.muted, 'Mute period expired.').then(() => {
-            removeProfileData(user, 'member');
-          }).catch(err => {
-            log(util.inspect(user));
-            errHandler(err, user);
-          });
-        }
-      }
-
-      function removeProfileData(user, type) {
-        OptiBotUtilities.getProfile(id, false).then(profile => {
-          if (profile) {
-            /* let entry = new RecordEntry()
-                        .setMod(bot.user.id)
-                        .setAction('mute')
-                        .setActionType('remove')
-                        .setReason(bot.user, `Mute period expired.`)
-                        .setParent(bot.user, profile.edata.mute.caseID)
-        
-                        if(!profile.edata.record) profile.edata.record = [];
-                        profile.edata.record.push(entry.raw); */
-
-            delete profile.edata.mute;
-
-            OptiBotUtilities.updateProfile(profile).then(() => {
-              finish(user, type);
-            }).catch(err => {
-              errHandler(err, user);
-            });
-          } else {
-            finish(user, type);
-          }
-        }).catch(err => {
-          errHandler(err, user);
-        });
-      }
-
-      function finish(user, type) {
-        for (let i = 0; i < Memory.mutes.length; i++) {
-          const mute = Memory.mutes[i];
-          if (mute.id === id) {
-            Memory.mutes.splice(i, 1);
-          }
-        }
-
-        new LogEntry({ channel: 'moderation' })
-          .setColor(bot.cfg.embed.default)
-          .setIcon(Assets.getEmoji('ICO_unmute').url)
-          .setTitle('Member Unmuted', 'Member Mute Removal Report')
-          .setHeader('Reason: Mute period expired.')
-          .addSection('Member Unmuted', (type === 'user') ? user : user.user)
-          .setThumbnail(((type === 'user') ? user : user.user).displayAvatarURL({ format: 'png' }))
-          .submit().then(() => {
-            resolve();
-          }).catch(err => {
-            OptiBotUtilities.err(err);
-            resolve();
-          });
-      }
-    });
   }
 
   /**
---
+++ modules/core/OptiBotUtil/unmuter.js
@@ -0,0 +1,120 @@
+const Memory = require('../OptiBotMemory.js');
+const OptiBotUtilities = require('../OptiBotUtil');
+const Assets = require('../OptiBotAssetsManager.js');
+const LogEntry = require('../OptiBotLogEntry.js');
+const djs = require('discord.js');
+const util = require('util');
+
+module.exports = (id) => {
+  const bot = Memory.core.client;
+  const log = bot.log;
+
+  // eslint-disable-next-line no-unused-vars
+  return new Promise((resolve, reject) => {
+    const errHandler = (err, user) => {
+      OptiBotUtilities.err(err);
+
+      let type = null;
+      if (user) type = (user.constructor === djs.User) ? 'user' : 'member';
+
+      const logEntry = new LogEntry({ channel: 'moderation' })
+        .setColor(bot.cfg.embed.error)
+        .setIcon(Assets.getEmoji('ICO_error').url)
+        .setTitle('Member Unmute Failure', 'Member Mute Removal Failure Report')
+        .setHeader('An error occurred while trying to unmute a user.')
+        .setDescription(`\`\`\`diff\n-${err}\`\`\``);
+
+      if (user) {
+        logEntry.addSection('Member', (type === 'user') ? user : user.user)
+          .setThumbnail(((type === 'user') ? user : user.user).displayAvatarURL({ format: 'png' }));
+      } else {
+        logEntry.addSection('Member', 'Unknown. (Error occurred before or during fetch operation)');
+      }
+
+      logEntry.submit().then(() => {
+        resolve();
+      }).catch(err => {
+        OptiBotUtilities.err(err);
+        resolve();
+      });
+    };
+
+    bot.mainGuild.members.fetch(id).then(mem => {
+      removeRole(mem);
+    }).catch(err => {
+      if (err.message.match(/invalid or uncached|unknown member|unknown user/i)) {
+        bot.users.fetch(id).then(user => {
+          removeRole(user);
+        }).catch(err => {
+          errHandler(err);
+        });
+      } else {
+        errHandler(err);
+      }
+    });
+
+    function removeRole(user) {
+      if (user.constructor === djs.User) {
+        removeProfileData(user, 'user');
+      } else {
+        user.roles.remove(bot.cfg.roles.muted, 'Mute period expired.').then(() => {
+          removeProfileData(user, 'member');
+        }).catch(err => {
+          log(util.inspect(user));
+          errHandler(err, user);
+        });
+      }
+    }
+
+    function removeProfileData(user, type) {
+      OptiBotUtilities.getProfile(id, false).then(profile => {
+        if (profile) {
+          /* let entry = new RecordEntry()
+                      .setMod(bot.user.id)
+                      .setAction('mute')
+                      .setActionType('remove')
+                      .setReason(bot.user, `Mute period expired.`)
+                      .setParent(bot.user, profile.edata.mute.caseID)
+      
+                      if(!profile.edata.record) profile.edata.record = [];
+                      profile.edata.record.push(entry.raw); */
+
+          delete profile.edata.mute;
+
+          OptiBotUtilities.updateProfile(profile).then(() => {
+            finish(user, type);
+          }).catch(err => {
+            errHandler(err, user);
+          });
+        } else {
+          finish(user, type);
+        }
+      }).catch(err => {
+        errHandler(err, user);
+      });
+    }
+
+    function finish(user, type) {
+      for (let i = 0; i < Memory.mutes.length; i++) {
+        const mute = Memory.mutes[i];
+        if (mute.id === id) {
+          Memory.mutes.splice(i, 1);
+        }
+      }
+
+      new LogEntry({ channel: 'moderation' })
+        .setColor(bot.cfg.embed.default)
+        .setIcon(Assets.getEmoji('ICO_unmute').url)
+        .setTitle('Member Unmuted', 'Member Mute Removal Report')
+        .setHeader('Reason: Mute period expired.')
+        .addSection('Member Unmuted', (type === 'user') ? user : user.user)
+        .setThumbnail(((type === 'user') ? user : user.user).displayAvatarURL({ format: 'png' }))
+        .submit().then(() => {
+          resolve();
+        }).catch(err => {
+          OptiBotUtilities.err(err);
+          resolve();
+        });
+    }
+  });
+};

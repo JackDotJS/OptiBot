af3325deb3f07937641b64a57e73aa1695c0da2f
akii0008 <jennasisis@gmail.com>
2020-09-13 14:57:26 -0400

    moved setWindowTitle into its own file

--- modules/core/OptiBotUtil.js
+++ modules/core/OptiBotUtil.js
@@ -6,49 +6,13 @@ const LogEntry = require('./OptiBotLogEntry.js');
 const Memory = require('./OptiBotMemory.js');
 const Assets = require('./OptiBotAssetsManager.js');
 
+module.exports = {
+  setWindowTitle: require('./OptiBotUtil/setWindowTitle')
+};
+
 module.exports = class OptiBotUtilities {
   constructor() {
     throw new Error('Why are you doing this? (Cannot instantiate this class.)');
-  }
-
-  static setWindowTitle(text) {
-    const bot = Memory.core.client;
-
-    if (text !== undefined) Memory.wintitle = text;
-
-    function statusName(code) {
-      if (code === 0) return 'READY';
-      if (code === 1) return 'CONNECTING';
-      if (code === 2) return 'RECONNECTING';
-      if (code === 3) return 'IDLE';
-      if (code === 4) return 'NEARLY';
-      if (code === 5) return 'DISCONNECTED';
-    }
-
-    const wintitle = [
-      `OptiBot ${bot.version}`,
-      `OP Mode ${bot.mode}`,
-    ];
-
-    if (bot.ws) {
-      let code = bot.ws.status;
-
-      if (bot.ws.shards.size > 0) code = bot.ws.shards.first().status;
-
-      wintitle.push(
-        `${Math.round(bot.ws.ping)}ms`,
-        `WS Code ${code} (${statusName(code)})`
-      );
-    } else {
-      wintitle.push(
-        '-0ms',
-        `WS Code 3 (${statusName(3)})`
-      );
-    }
-
-    if (typeof Memory.wintitle === 'string') wintitle.push(Memory.wintitle);
-
-    process.title = wintitle.join(' | ');
   }
 
   static parseInput(text) {

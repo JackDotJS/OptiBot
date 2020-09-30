c430325138758654025eb9340137afcfd7b9a7ab
realjackasterisk <wingedasterisk@gmail.com>
2020-09-29 21:06:20 -0700

    fucking

--- update.bat
+++ update.bat
@@ -5,7 +5,7 @@ SET /P AREYOUSURE=Force re-download OptiBot? (Y/[N])
 IF /I "%AREYOUSURE%" NEQ "Y" GOTO END
 
 git fetch --all
-git reset --hard origin/nx
+git reset --hard origin/master
 call npm install
 
 :END

dd09f69b8d5d22c377390df419c244852e813b3d
Kyle Edwards <wingedasterisk@gmail.com>
2021-03-25 13:34:20 -0700

    simplified !dr and command class

--- modules/cmd/dr.js
+++ modules/cmd/dr.js
@@ -1,7 +1,7 @@
 const util = require(`util`);
 const path = require(`path`);
 const djs = require(`discord.js`);
-const request = require(`request`);
+const fetch = require(`node-fetch`);
 const { Command, memory, Assets } = require(`../core/optibot.js`);
 
 const bot = memory.core.client;
@@ -22,165 +22,48 @@ const metadata = {
     ].join(`\n`)
   },
   args: `<e-mail> <token>`,
+  guilds: [ bot.cfg.guilds.optifine ],
   dm: true,
   image: `IMG_token`,
-  flags: [ `DM_ONLY`, `STRICT`, `DELETE_ON_MISUSE`, `NO_LOGGING_ARGS`, `LITE` ],
-  run: null
+  flags: [ `DM_ONLY`, `STRICT`, `DELETE_ON_MISUSE`, `NO_LOGGING_ARGS`, `LITE` ]
 };
 
-metadata.run = (m, args, data) => {
-  const donator = {
-    guild: bot.guilds.cache.get(bot.cfg.guilds.donator),
-    member: null,
-    ban: null,
-  };
-  const inviteSettings = {
-    temporary: true,
-    maxUses: 1,
-    unique: true,
-    reason: `Created for ${m.author.tag} (${m.author.id})`
-  };
-
-  donator.guild.members.fetch(m.author.id).then(dmem => {
-    donator.member = dmem;
-    checkBan();
-  }).catch(err => {
-    if (err.message.match(/invalid or uncached|unknown member|unknown user/i)) {
-      checkBan();
-    } else {
-      bot.util.err(err, { m: m });
-    }
-  });
-
-  function checkBan() {
-    donator.guild.fetchBan(m.author.id).then(ban => {
-      donator.ban = ban;
-      final();
-    }).catch(err => {
-      if (err.message.match(/unknown ban/i)) {
-        final();
-      } else {
-        bot.util.err(err, { m: m });
-      }
-    });
+const exec = async (m, args, data) => {
+  if (!args[0]) {
+    return bot.util.missingArgs(m, metadata);
+  }
+  
+  if (args[0].indexOf(`@`) < 0 && args[0].indexOf(`.`) < 0) {
+    return bot.util.err(`You must specify a valid e-mail address.`, { m });
+  } 
+  
+  if (!args[1]) {
+    return bot.util.err(`You must specify your donator token.`, { m });
   }
 
-  const getDonatorInvite = () => {
-    return new Promise((resolve, reject) => {
-      const cached = memory.donatorInvites[m.author.id];
+  const res = await fetch(
+    `https://optifine.net/validateToken?e=` + encodeURIComponent(args[0]) + `&t=` + encodeURIComponent(args[1]),
+    { headers: { 'User-Agent': `optibot` } }
+  ).catch(err => bot.util.err(err));
 
-      if (cached) {
-        donator.guild.fetchInvites().then(invites => {
-          const invite = invites.get(cached.code);
+  if (res == null || res.status !== 200) return bot.util.err(new Error(`Failed to get a response from the OptiFine API`), { m });
 
-          log(util.inspect(invite));
+  const body = await res.text();
 
-          if (!invite || invite.uses === invite.maxUses) {
-            resolve(null);
-          } else {
-            resolve(invite);
-          }
-        });
-      } else {
-        donator.guild.channels.cache.get(`686207354315735071`).createInvite(inviteSettings).then(invite => {
-          memory.donatorInvites[m.author.id] = invite;
+  if (body === `true`) {
+    await data.member.roles.add([bot.cfg.roles.donator, bot.cfg.roles.donatorColor], `Donator status verified.`);
 
-          resolve(invite);
-        }).catch(err => {
-          reject(err);
-        });
-      }
-    });
-  };
+    const embed = new djs.MessageEmbed()
+      .setColor(bot.cfg.embed.okay)
+      .setAuthor(`Thank you for your contribution! Your donator role has been granted.`, Assets.getEmoji(`ICO_okay`).url);
 
-  function final() {
-    if (data.member.roles.cache.has(bot.cfg.roles.donator)) {
-      if (donator.member) {
-        const embed = bot.util.err(`You already have the Donator role!`)
-          .setDescription(`Additionally, you cannot get an invite to the Donator Discord server because you're already a member.`);
+    m.channel.send({ embed: embed });
+  } else {
+    const embed = bot.util.err(`Invalid credentials.`)
+      .setDescription(`Make sure that your token and e-mail are the same as what you see on https://optifine.net/login.`);
 
-        m.channel.send({ embed: embed });
-      } else if (donator.ban) {
-        const embed = bot.util.err(`You already have the Donator role!`)
-          .setDescription(`Additionally, you cannot get an invite to the Donator Discord server because you've been banned.`);
-
-        m.channel.send({ embed: embed });
-      } else {
-        getDonatorInvite().then(invite => {
-          const embed = new djs.MessageEmbed()
-            .setColor(bot.cfg.embed.default)
-            .setAuthor(`You already have the Donator role!`, Assets.getEmoji(`ICO_info`).url);
-
-          if (invite) {
-            embed.setDescription(`However, it seems you're not yet a member of the Donator Discord server. Here's an invite!\n${invite.url}`);
-
-            if (donator.guild.icon) {
-              embed.setThumbnail(donator.guild.iconURL({ format: `png` }));
-            }
-          } else {
-            embed.setDescription(`However, it seems you're not yet a member of the Donator Discord server. Unfortunately, it seems you've already used your invite link for the day. If you believe this is a mistake, please contact an administrator. Thank you!`);
-          }
-
-          m.channel.send({ embed: embed });
-        });
-      }
-    } else if (!args[0]) {
-      bot.util.missingArgs(m, metadata);
-    } else if (args[0].indexOf(`@`) < 0 && args[0].indexOf(`.`) < 0) {
-      bot.util.err(`You must specify a valid e-mail address.`, { m: m });
-    } else if (!args[1]) {
-      bot.util.err(`You must specify your donator token.`, { m: m });
-    } else {
-      request({ url: `https://optifine.net/validateToken?e=` + encodeURIComponent(args[0]) + `&t=` + encodeURIComponent(args[1]), headers: { 'User-Agent': `optibot` } }, (err, res, body) => {
-        if (err || !res || !body || res.statusCode !== 200) {
-          bot.util.err(err || new Error(`Failed to get a response from the OptiFine API`), { m: m });
-        } else if (body === `true`) {
-          data.member.roles.add([bot.cfg.roles.donator, bot.cfg.roles.donatorColor], `Donator status verified.`).then(() => {
-            if (donator.member) {
-              const embed = new djs.MessageEmbed()
-                .setColor(bot.cfg.embed.okay)
-                .setAuthor(`Thank you for your contribution! Your donator role has been granted.`, Assets.getEmoji(`ICO_okay`).url)
-                .setDescription(`Please note, your token has been reset. Additionally, you cannot get an invite to the Donator Discord server because you're already a member.`);
-
-              m.channel.send({ embed: embed });
-            } else if (donator.ban) {
-              const embed = new djs.MessageEmbed()
-                .setColor(bot.cfg.embed.okay)
-                .setAuthor(`Thank you for your contribution! Your donator role has been granted.`, Assets.getEmoji(`ICO_okay`).url)
-                .setDescription(`Please note, your token has been reset. Additionally, you cannot get an invite to the Donator Discord server because you've been banned.`);
-
-              m.channel.send({ embed: embed });
-            } else {
-              getDonatorInvite().then(invite => {
-                const embed = new djs.MessageEmbed()
-                  .setColor(bot.cfg.embed.okay)
-                  .setAuthor(`Thank you for your contribution! Your donator role has been granted.`, Assets.getEmoji(`ICO_okay`).url);
-
-                if (invite) {
-                  embed.setDescription(`You're now qualified to join the Donator Discord server. If you're interested, here's an invite!\n${invite.url}`);
-
-                  if (donator.guild.icon) {
-                    embed.setThumbnail(donator.guild.iconURL({ format: `png` }));
-                  }
-
-                } else {
-                  embed.setDescription(`You're now qualified to join the Donator Discord server. Unfortunately, it seems you've already used your invite link for the day. If you believe this is a mistake, please contact an administrator. Thank you!`);
-                }
-
-                m.channel.send({ embed: embed });
-              });
-            }
-
-          });
-        } else {
-          const embed = bot.util.err(`Invalid credentials.`)
-            .setDescription(`Make sure that your token and e-mail are the same as what you see on https://optifine.net/login.`);
-
-          m.channel.send({ embed: embed });
-        }
-      });
-    }
+    m.channel.send({ embed: embed });
   }
 };
 
-module.exports = new Command(metadata);
+module.exports = new Command(metadata, exec);
--- modules/core/command.js
+++ modules/core/command.js
@@ -3,23 +3,7 @@ const util = require(`util`);
 const djs = require(`discord.js`);
 const memory = require(`./memory.js`);
 
-/**
- * OptiBot Core: Command Class
- */
 module.exports = class Command {
-
-  /**
-     * @param {Object} data Command Data
-     * @param {String} data.name Name of this command.
-     * @param {Array[String]} data.aliases Command aliases.
-     * @param {String} [data.short_desc] Short description.
-     * @param {String} [data.long_desc] Full, long description.
-     * @param {(String|Array[String])} [data.args] Arguments/usage example(s).
-     * @param {String} [data.image] Thumbnail image filename.
-     * @param {Number} [data.authlvl] Minimum authlvl.
-     * @param {Array[String]} [data.flags] Restriction and modifier flags.
-     * @param {Function} data.run The actual command function.
-     */
   constructor(meta, exec) {
     const bot = memory.core.client;
     const log = bot.log;
@@ -69,6 +53,12 @@ module.exports = class Command {
     if (!Array.isArray(flags)) {
       throw new TypeError(`Invalid Command property: flags`);
     }
+    if (!dm && flags.includes(`DM_ONLY`)) {
+      throw new Error(`Command "${name}": Cannot use flag DM_ONLY while this command does not allow DMs.`);
+    }
+    if (flags.includes(`BOT_CHANNEL_ONLY`) && flags.includes(`DM_ONLY`)) {
+      throw new Error(`Command "${name}": Flags BOT_CHANNEL_ONLY and DM_ONLY are mutually exclusive.`);
+    }
 
     const metadata = {
       name: name,
@@ -79,86 +69,13 @@ module.exports = class Command {
       },
       args: [],
       guilds: [...new Set(guilds)],
-
-      // Image to be shown as a thumbnail when this command is viewed through !help.
-      // Must be a plain string specifying a complete filename from the ../assets/img directory.
-      // i.e. "IMG_token.png"
       image: image,
-
-      // Allow usage of command in DMs.
       dm: dm,
-
-      flags: {
-        // Command can ONLY be used in Direct Messages
-        DM_ONLY: false,
-
-        // Command can only be used in the designated bot channels/categories, if not in a DM. (see: config.channels.bot)
-        // Mutually exclusive with DM_ONLY and NO_DM
-        BOT_CHANNEL_ONLY: false,
-
-        // Command can only be used staff-only channels/categories. (see: config.channels.staff)
-        STAFF_CHANNEL_ONLY: false,
-
-        // Normally, users with the bypassChannels permission node are exempt from the BOT_CHANNEL_ONLY flag.
-        // Additionally, users with the wildcard (*) permission node are exempt from the STAFF_CHANNEL_ONLY flag.
-        // This flag makes it so all restrictions are applied, regardless of permissions.
-        STRICT: false, 
-
-        // Deletes the users message if any restriction results in the command not firing.
-        // Useful for reducing spam, or for commands that require particularly sensitive information as arguments.
-        DELETE_ON_MISUSE: false,
-
-        // Prevents command from showing in !help command list. (does not apply for users with bypassHidden permission node)
-        // When combined with the PERMS_REQUIRED flag, the bot will also
-        // treat this command as non-existent when any user attempts to use
-        // it without the required permissions.
-        HIDDEN: false,
-
-        // Usage of this command will not be logged at all.
-        NO_LOGGING: false,
-
-        // Arguments for this command will not be logged.
-        NO_LOGGING_ARGS: false,
-
-        // Allows command to be loaded during Modes 1 and 2.
-        LITE: false,
-        
-        // Requires the user to have a permission group, inherited or otherwise, that allows usage of this command.
-        PERMS_REQUIRED: false,
-      }
+      flags: [...new Set(flags)]
     };
 
-    if (flags.length > 0) {
-      flags = [...new Set(flags)];
-      let found = 0;
-
-      flags.forEach(t => {
-        if (typeof t !== `string`) {
-          throw new TypeError(`Flags must be specified as strings.`);
-        } else
-        if (typeof metadata.flags[t.toUpperCase()] === `boolean`) {
-          metadata.flags[t.toUpperCase()] = true;
-          found++;
-        }
-      });
-
-      if (found === 0) {
-        throw new Error(`All given flags are invalid.`);
-      }
-
-      if (!metadata.dm && metadata.flags[`DM_ONLY`]) {
-        throw new Error(`Command "${name}": Cannot use flag DM_ONLY while this command does not allow DMs.`);
-      }
-
-      if (metadata.flags[`BOT_CHANNEL_ONLY`] && metadata.flags[`DM_ONLY`]) {
-        throw new Error(`Command "${name}": Flags BOT_CHANNEL_ONLY and DM_ONLY are mutually exclusive.`);
-      }
-    }
-
     if (Array.isArray(args) && args.length > 0) {
-      for (const arg of args) {
-        metadata.args.push(`${bot.prefix}${name} ${arg}`);
-      }
+      for (const arg of args) metadata.args.push(`${bot.prefix}${name} ${arg}`);
     } else
     if (typeof args === `string`) {
       metadata.args.push(`${bot.prefix}${name} ${args}`);

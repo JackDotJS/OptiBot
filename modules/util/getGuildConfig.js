79701ea6f278f2d05d6694b057f2cc6db4295469
akii0008 <samir.js.zip@gmail.com>
2021-05-06 21:58:54 -0400

    getGuildConfig: spread syntax on sources to avoid array assignment

--- modules/util/getGuildConfig.js
+++ modules/util/getGuildConfig.js
@@ -1,19 +1,18 @@
-const djs = require(`discord.js`);
 const memory = require(`../core/memory.js`);
 
 const bot = memory.core.client;
 const log = bot.log;
 
-
 module.exports = async (guild) => {
   if (guild == null) return null;
 
   const cfg = {};
-  const sources = [ bot.gcfg ];
+  const sources = [bot.gcfg];
 
   const docs = await memory.db.guilds.find({ _id: bot.guilds.resolveID(guild) });
 
   if (docs.length > 0) sources.push(docs[0]);
 
-  return Object.assign(cfg, sources);
+  //console.log('object', Object.assign(cfg, ...sources));
+  return Object.assign(cfg, ...sources);
 };
